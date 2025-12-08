1a8804a6915e05ea4ddae039247e35b767f34738
patricia <patricia@korindo.co.id>
2025-12-08 15:08:52 +0700

    u

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from "cors";
-import pool from "./db.js";
+import pool from "./db.js"; 
 import authRoutes from './routes/authRoutes.js'
 import categoriesRoutes from './routes/categoriesRoutes.js'
 import moneyAccountsRoutes from './routes/moneyAccountsRoutes.js'
@@ -14,19 +14,18 @@ app.use(cors());
 app.use(express.json());
 
 //Routes
-app.use('/auth', authRoutes)
-app.use('/categoriesRoutes', middleware, categoriesRoutes)
-app.use('/moneyAccountsRoutes', middleware, moneyAccountsRoutes)
-app.use('/targetsRoutes', middleware, targetsRoutes)
-app.use('/transactionsRoutes', middleware, transactionsRoutes)
+app.use('/auth',authRoutes)
+app.use('/categoriesRoutes',middleware,categoriesRoutes)
+app.use('/moneyAccountsRoutes',middleware,moneyAccountsRoutes)
+app.use('/targetsRoutes',middleware,targetsRoutes)
+app.use('/transactionsRoutes',middleware,transactionsRoutes)
 
-app.get('/', (async (req, res) => {
-    const result = await pool.query("SELECT * FROM users");
-    res.json(result.rows);
+app.get('/',((req,res)=>{
+    res.sendStatus(200)
 }))
 
 
 
-app.listen(PORT, () => {
+app.listen(PORT,()=>{
     console.log(`Server started at http://localhost:${PORT}`)
 })
